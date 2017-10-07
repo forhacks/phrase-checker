e0e479423b1505f74b426bdd9967f03ec90332f8
thegithubgeek <wfrankw9@gmail.com>
2017-10-06 18:06:45 -0700

    Add data processing into neuralnet

--- neuralnet.py
+++ neuralnet.py
@@ -3,6 +3,8 @@ import numpy as np
 
 # hyperparams
 max_len = 25
+epochs = 1000
+learning_rate = 0.01
 
 
 def process_def(definition):
@@ -28,5 +30,52 @@ data = np.reshape(data, (-1, 3)).T
 x = data[:2]
 y = map(int, data[2:][0])
 
-print(x)
+model = Word2Vec.load("word2vec model/trained/trained.w2v")
 
+x = [[model.wv[word] for word in a] for a in x]
+x = np.array([np.append(a[0], a[1]) for a in x])
+
+# init weights and the bias
+w = np.zeros((1, max_len))
+b = 0
+
+
+def sigmoid(f_a):
+    return 1 / (1 + np.exp(-f_a))
+
+
+print '-- STARTING TRAINING --'
+
+for i in range(epochs):
+    # init loss
+    j = 0
+
+    # multiply inputs by weights and and bias
+    z = x.dot(w.transpose()) + b
+
+    # take sigmoid
+    a = sigmoid(z)
+    # calculate the loss
+    j += -np.sum(np.multiply(y, np.log(a)) + np.multiply(1 - y, np.log(1 - a))) / len(x)
+
+    # derivative of z
+    dz = a - y
+
+    # update w and b
+    w -= learning_rate * (np.sum(np.multiply(dz, x), axis=0) / len(x))
+    b -= np.sum(dz) / len(x)
+
+    # print loss
+    print "Loss: " + str(j)
+
+print
+print '-- STARTING TESTING --'
+print
+
+# repeat above steps for test data
+test_x = x
+
+test_y = y
+
+z = x.dot(w.transpose()) + b
+a = sigmoid(z)
