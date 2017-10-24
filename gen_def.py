e59a3a0b4bb0068d0de86e789b952476d58aee79
Oliver Ni <oliver.ni@gmail.com>
2017-10-24 07:27:25 -0700

    Add definitions with 0

--- gen_def.py
+++ gen_def.py
@@ -4,13 +4,13 @@
 # do words that are not same
 # save in array.
 
-import requests, time
+import requests, time, random
 from xml.etree import ElementTree
 from multiprocessing import Pool
 
-ox_id = "aa0b9797"  # ENTER_OX_APP_ID
-ox_key = "b8da3ef45aa7394e62d3d73fe9b197c5"  # ENTER_OX_APP_KEY
-mw_key = ""  # ENTER_MW_THESAURUS_KEY
+ox_id = "83a8d0b7"  # ENTER_OX_APP_ID
+ox_key = "a41bec7e5a3e6d9f7f3e738498d42def"  # ENTER_OX_APP_KEY
+mw_key = "90cecf59-d31f-4c48-9286-ee48d249bdcb"  # ENTER_MW_THESAURUS_KEY
 
 assert ox_id != ""
 assert ox_key != ""
@@ -23,7 +23,12 @@ headers = {
 }
 
 word_file = open("words.txt", "r")
-word_list = word_file.read().split()
+word_list1 = word_file.read().split()
+word_list2 = list(word_list1)
+
+random.shuffle(word_list1)
+random.shuffle(word_list2)
+
 word_file.close()
 
 
@@ -55,12 +60,17 @@ def get_mw_def(f_word):
         return None
 
 
-def get_pair(word):
+def get_pair(i):
+
+    ox_word = word_list1[i]
+    mw_word = word_list2[i]
 
     try:
 
-        ox_def = get_ox_def(word)
-        mw_def = get_mw_def(word)
+        ox_def = get_ox_def(ox_word)
+        mw_def = get_mw_def(mw_word)
+
+        print(ox_def, mw_def)
 
         if ox_def is not None and mw_def is not None:
             return ox_def, mw_def
@@ -74,23 +84,23 @@ p = Pool()
 out = open('gen_def.txt', 'a')
 interval = 5
 
-for i in range(1900, 5000, interval):
+for i in range(0, 5000, interval):
 
-    print 'Starting words', i+1, '-', i+interval
-    a = [x for x in p.map(get_pair, word_list[i:i+interval]) if x is not None]
+    print('Starting words', i+1, '-', i+interval)
+    a = [x for x in p.map(get_pair, range(i, i+interval)) if x is not None]
     tries = 0
 
     while len(a) == 0 and tries <= 4:
-        print 'Received no results. Retrying...'
+        print('Received no results. Retrying...')
         time.sleep(20)
-        a = [x for x in p.map(get_pair, word_list[i:i+interval]) if x is not None]
+        a = [x for x in p.map(get_pair, range(i, i+interval)) if x is not None]
         tries += 1
     if tries == 4:
-        print 'Continuing after 4 tries.'
+        print('Continuing after 4 tries.')
 
     out.write('\n'.join('\n'.join(x + ('1',)) for x in a))
     out.write('\n')
-    print 'Finished words', i+1, '-', i+interval, 'with', len(a), 'results'
+    print('Finished words', i+1, '-', i+interval, 'with', len(a), 'results')
     print
 
 out.close()
--- gen_def2.py
+++ gen_def2.py
@@ -8,13 +8,14 @@ def get_wapi_text(word):
     url = "https://wordsapiv1.p.mashape.com/words/" + word + "/definitions"
     r = requests.get(url)
 
-    print r.status_code
+    print(r.status_code)
 
     try:
         j = r.json()
     except ValueError:
         return None
 
-    print j
+    print(j)
+
 
 get_wapi_text("word")
